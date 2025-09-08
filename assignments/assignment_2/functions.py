030368d35166bb708f7458764f451c12bfab841c
Alex Yourlastname <ibrahimali04@icloud.com>
2025-09-08 21:35:47 +0200

    Assignment 2 - cropping complete

--- assignments/assignment_2/functions.py
+++ assignments/assignment_2/functions.py
@@ -15,11 +15,35 @@ def padding(image, border_width, out_dir, out_name="padded.png"):
     cv2.imwrite(os.path.join(out_dir, out_name), padded)
     return padded
 
-def cropping(image, x0, x1, y0, y1, out_dir, out_name="cropped.png"):
+def cropping(image, x_0, x_1, y_0, y_1, out_dir, out_name="cropped.png"):
     check_dir(out_dir)
     h, w = image.shape[:2]
-    x0, y0 = max(0, x0), max(0, y0)
-    x1, y1 = min(w, x1), min(h, y1)
-    cropped = image[y0:y1, x0:x1]
+    x_0, y_0 = max(0, x_0), max(0, y_0)
+    x_1, y_1 = min(w, x_1), min(h, y_1)
+    cropped = image[y_0:y_1, x_0:x_1]
     cv2.imwrite(os.path.join(out_dir, out_name), cropped)
     return cropped
+
+def resizing(image, width, height, out_dir, out_name="resized.png"):
+    check_dir(out_dir)
+    resized = cv2.resize(image, (width, height), interpolation=cv2.INTER_AREA)
+    cv2.imwrite(os.path.join(out_dir, out_name.format(width, height)), resized)
+    return resized
+
+def copying(image, emptyPicArray, out_dir, out_name="copied.png"):
+    check_dir(out_dir)
+    h, w = image.shape[:2]
+    if image.ndim == 2:
+        for y in range(h):
+            for x in range(w):
+                emptyPicArray[y, x] = image[y, x]
+    else:
+        c = image.shape[2]
+        for y in range(h):
+            for x in range(w):
+                for k in range(c):
+                    emptyPicArray[y, x, k] = image[y, x, k]
+    cv2.imwrite(os.path.join(out_dir, out_name), emptyPicArray)
+    return emptyPicArray
+
+
--- assignments/assignment_2/main.py
+++ assignments/assignment_2/main.py
@@ -1,9 +1,10 @@
 import os
 import cv2
 import numpy as np
+from numpy.ma.core import empty
 
 from functions import (
-    padding, cropping,
+    padding, cropping,resizing, copying,
 )
 
 BASE = r"C:\Users\ibrah\Python\ikt213\assignments\assignment_2"
@@ -17,12 +18,18 @@ if image is None:
     raise ValueError("Image could not be loaded")
 
 # 1) Padding with border width 100px
-#padded = padding(image, border_width=100, out_dir=BASE_OUT)
+padded = padding(image, border_width=100, out_dir=BASE_OUT)
 
-#2) Cropping, 80px from top-left, 130 from bottom-right
+# 2) Cropping, 80px from top-left, 130 from bottom-right
 h, w = image.shape[:2]
 x0, y0 = 80, 80
 x1, y1 = w - 130, h - 130
 cropped = cropping(image, x0, x1, y0, y1, out_dir=BASE_OUT)
 
+# 3) Resizing to 200x200
+resized = resizing(image, width=200, height=200, out_dir=BASE_OUT)
+
+# 4) Manually copying into empty array
+empty = np.zeros_like(image, dtype=np.uint8)
+copied = copying(image, empty, out_dir=BASE_OUT)
 
