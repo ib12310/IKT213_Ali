5ec677180d6f153ff1bd6145ba82687e849b4b91
Alex Yourlastname <ibrahimali04@icloud.com>
2025-09-08 21:20:48 +0200

    Assignment 2 - padded complete

--- assignments/assignment_2/main.py
+++ assignments/assignment_2/main.py
@@ -4,7 +4,14 @@ import numpy as np
 
 from functions import padding
 
-BASE = r"assignment_2"
+BASE = r"C:\Users\ibrah\Python\ikt213\assignments\assignment_2"
 BASE_OUT = os.path.join(BASE, "solutions")
 os.makedirs(BASE_OUT, exist_ok=True)
 
+img_path = os.path.join(BASE_OUT, "lena-2.png")
+image = cv2.imread(img_path)
+
+if image is None:
+    raise ValueError("Image could not be loaded")
+
+padded = padding(image, border_width=100, out_dir=BASE_OUT)
